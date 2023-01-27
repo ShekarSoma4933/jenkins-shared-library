55194efe445ea19657d70e845043d867a12d24c8
ShekarSoma4933 <shekarsoma4933@gmail.com>
2023-01-27 03:13:53 -0500

    add params to  buildImage file

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "this is push stage"
     withCredentials([usernamePassword('credentialsId': 'nexus-repo-credentials', 'usernameVariable': 'USER', 'passwordVariable': 'PASS')]) {
-        sh "docker build -t 143.198.43.144:8083/maven-web-app:2.2 ."
+        sh "docker build -t ${imageName} ."
         sh "echo ${PASS} | docker login -u ${USER} --password-stdin 143.198.43.144:8083"
-        sh "docker push 143.198.43.144:8083/maven-web-app:2.2"
+        sh "docker push ${imageName}"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call() {
-    echo "this is buildJar stage"
+    echo "this is buildJar stage for ${BRANCH_NAME} branch"
     sh "mvn package"
 }
